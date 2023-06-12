80c914f07c686af535f0957372f8f40c7470affb
krsnarocham <krsnarochawork@gmail.com>
2023-06-12 07:30:44 -0700

    Initial commit

---
+++ src/test/java/com/example/pruebacontrollers/PruebaControllersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebacontrollers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaControllersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
